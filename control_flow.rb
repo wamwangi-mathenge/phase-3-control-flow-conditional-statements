b85aad80ea68dbe5374e1e977c72b2d95d0871a1
Brian <wamwangimathenge@gmail.com>
2023-02-07 17:54:27 +0300

    First & Second Tests Passing

--- control_flow.rb
+++ control_flow.rb
@@ -1,9 +1,25 @@
 def admin_login(username, password)
   # your code here
+  if username == "admin" && password == "12345"
+    "Access granted"
+  elsif username == "ADMIN" && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
   # your code here
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 65
+    "It's a little chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
